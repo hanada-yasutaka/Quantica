9d5af098900a297a63c2c01ca1f6d917185ef890
hanada-yasutaka <hanada1985@gmail.com>
2013-12-25 08:49:25 +0900

    add some files

---
+++ PacletInfo.m
@@ -0,0 +1,19 @@
+(* Paclet Info File *)
+
+(* created 2013/12/22*)
+
+Paclet[
+    Name -> "Quantica",
+    Version -> "0.2.0",
+    MathematicaVersion -> "6+",
+    Description -> "Calculator of quantum systems with mathematica",
+    Creator -> "Yasutaka Hanada",
+    Extensions -> 
+        {
+            {"Documentation", Resources -> 
+                {"Guides/Quantica"}
+            , Language -> "English", LinkBase -> "Quantica", MainPage -> "Quantica\n"}
+        }
+]
+
+
