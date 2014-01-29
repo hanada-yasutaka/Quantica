f64964349354de6dc14e2ddcfffa8dbde8e74b0e
Yasutaka Hanada <hanada@d175-m.k.nl.test>
2014-01-29 18:58:09 +0900

    modify savetxt path

--- Quantica/Util.m
+++ Quantica/Util.m
@@ -11,7 +11,7 @@ SaveState::usage="SaveState[x,vector,fname]"
 SaveEigenvalue::usage="SaveEigenvalue[eigenvalues,fname]"
 SaveEigen::usage="SaveEigen[eigenvalues,eigenvectors,fname->eigen, verbose->True]\n"
 SaveSplitting::usage"SaveSplitting[energy] save energy splitting"
-
+SetPath::PathError = "directory `1` is not exist"
 (*instantiation*)
 Util`SaveState      := Util`Private`SaveState
 Util`SaveEigenvalue := Util`Private`SaveEigenvalue
@@ -35,34 +35,40 @@ WriteScaleInfo[str_] := Module[{d},
     Export[str,"# PLANCK: "<> ToString[N[Planck,10]] <>" \n"];
     Export[str,"# DIM: "<> ToString[Dim] <> " \n"];
 ]
-Options[SaveState] = {Verbose->False}
-SaveState[x_,evec_,fname_,OptionsPattern[]] := Module[{str},
-    str=OpenWrite[fname];
+SetPath[path_]:=If[DirectoryQ[path], SetDirectory[path];True,Message[SetPath::PathError, path];False ];
+
+Options[SaveState] = {Verbose->False, Path->Directory[]}
+SaveState[x_,evec_,fname_,OptionsPattern[]] := Module[{str,path},
+	path = OptionValue[Path];
+	If[DirectoryQ[path], True, CreateDirectory[path]];
+    str=OpenWrite[path<>"/"<>fname];	
     WriteScaleInfo[str];
     Export[str,"# x, |<x|vec>|^2, Re[<x|vec>], Im[<x|vec>] \n"];        
     Export[str,Transpose[{x,Abs[evec*Conjugate[evec]],Re[evec],Im[evec]}]];
     Close[str];
-    If[OptionValue[Verbose],Print["outdate: "<>fname]]
+    If[OptionValue[Verbose],Print["outdate: "<>path<>"/"<>fname]]
 ]
-Options[SaveEigenvalue] = {Verbose->False}
-SaveEigenvalue[evals_,fname_,OptionsPattern[]] := Module[{str,ene,abs},
-    str=OpenWrite[fname];
+Options[SaveEigenvalue] = {Verbose->False,Path->Directory[]}
+SaveEigenvalue[evals_,fname_,OptionsPattern[]] := Module[{str,ene,abs,path},
+	path = OptionValue[Path];
+	If[DirectoryQ[path], True, CreateDirectory[path]];	
+    str=OpenWrite[path<>"/"<>fname];
     WriteScaleInfo[str];
     ene = I*Hbar*Log[evals]/Tau;
     abs = Abs[Conjugate[evals]*evals];
     Export[str,"# index, Re[val], Im[val], Re[q-ene], Im[q-ene], |conj(val)val|^2 \n"];
     Export[str,Transpose[{Range[0,Length[evals]-1], Re[evals], Im[evals], Re[ene], Im[ene], abs }] ];
     Close[str];
-    If[OptionValue[Verbose],Print["outdata: "<>fname]]
-    ]
+    If[OptionValue[Verbose],Print["outdata: "<>path <>"/"<>fname]]
+]
 
-Options[SaveEigen] = {Head->"eigen",Verbose->False}
-SaveEigen[evals_,evecs_,OptionsPattern[]] := Module[{fname,i},
-    fname=OptionValue[Head] <>"_evals.dat";
-    SaveEigenvalue[evals,fname, Verbose->OptionValue[Verbose]];
+Options[SaveEigen] = {Head->"eigen",Verbose->False,Path->Directory[]}
+SaveEigen[evals_,evecs_,opts:OptionsPattern[]] := Module[{fname,i},
+    fname= OptionValue[Head] <>"_evals.dat";
+    SaveEigenvalue[evals,fname, opts];
     For[i=0,i<Length[evals],i++;
         fname=OptionValue[Head] <>"_qrep_"<> ToString[i-1] <> ".dat";
-        SaveState[X[[1]], evecs[[i]], fname,Verbose->OptionValue[Verbose]];
+        SaveState[X[[1]], evecs[[i]], fname, opts];
     ]
 ]
 MirrorParity := State`MirrorParity
